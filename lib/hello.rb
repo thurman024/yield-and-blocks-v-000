3d96bc5966247e885235f1e1fc5bafc22db08fac
Stephen Thurman <thurman024@gmail.com>
2018-01-24 23:12:48 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -13,7 +13,7 @@ def hello_t(array)
 end
 
 # call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
+["Tim", "Tom", "Jim"] do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
